3caf19f1f40090e5e4b777ffe4832d88aacc8a6a
Mrriver <1209577113@qq.com>
2019-07-23 19:39:13 +0800

    提交配置

--- src/main/java/com/xq/tmall/TmallSpringBootApplication.java
+++ src/main/java/com/xq/tmall/TmallSpringBootApplication.java
@@ -3,14 +3,25 @@ package com.xq.tmall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @ServletComponentScan
 @MapperScan("com.xq.tmall.dao")
-public class TmallSpringBootApplication {
+public class TmallSpringBootApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TmallSpringBootApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TmallSpringBootApplication.class,args);
